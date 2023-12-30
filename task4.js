d670c018b46d82e573ac38702e3c998f4d5115e9
geersann <148795588+geersann@users.noreply.github.com>
2023-12-30 04:30:47 +0200

    Initial commit

---
+++ task4.js
@@ -0,0 +1,16 @@
+// На сторінці є форма "Новий користувач"
+// У форму вже введені дані користувача
+// Потрібно отримати дані з усіх полів та повернути та подати у вигляді наступних змінних
+// name, phoneNumber, dataOfBirth, emailAddress
+// 
+
+
+// Ваш код ..
+
+
+module.exports = {
+    name,
+    phoneNumber,
+    dataOfBirth,
+    emailAddress
+};
